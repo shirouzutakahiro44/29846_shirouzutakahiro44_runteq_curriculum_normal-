1887dd6a6cfd081baf745edf277d37d27c148c21
uzuhiro1 <uzuhiro1@gmail.com>
2023-08-01 22:22:08 +0900

    課題１修正

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,12 @@ module RunteqNormal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework false
+      g.skip_routes true
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
